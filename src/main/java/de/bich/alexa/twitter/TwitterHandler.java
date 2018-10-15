f7726bd7d157cc537bd5d8e252a06563a2947626
Nils Schwinning <nils.schwinning@gmx.de>
2018-10-15 22:23:26 +0200

    Adds twitter functionality

---
+++ src/main/java/de/bich/alexa/twitter/TwitterHandler.java
@@ -0,0 +1,69 @@
+package de.bich.alexa.twitter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+public class TwitterHandler {
+	
+	public static final String API_KEY_PROPERTY = "API_KEY";
+
+    public static final String API_SECRET_PROPERTY = "API_SECRET";
+
+    public static final String TOKEN_PROPERTY = "TOKEN_KEY";
+
+    public static final String TOKEN_SECRET_PROPERTY = "TOKEN_SECRET";
+
+    // security values needed for connecting to the twitter account (defined in config.properties)
+    private static String apiKey;
+
+    private static String apiSecret;
+
+    private static String token;
+
+    private static String tokenSecret;
+
+    // connection to twitter
+    private static Twitter twitter;
+
+
+	static {
+		final Properties prop = new Properties();
+
+        try (final InputStream input = TwitterHandler.class.getResourceAsStream("/config.properties")) {
+            // load a properties file
+            prop.load(input);
+
+            // get the property values
+            apiKey = prop.getProperty(API_KEY_PROPERTY);
+            apiSecret = prop.getProperty(API_SECRET_PROPERTY);
+            token = prop.getProperty(TOKEN_PROPERTY);
+            tokenSecret = prop.getProperty(TOKEN_SECRET_PROPERTY);
+            
+            twitter = TwitterFactory.getSingleton();
+            twitter.setOAuthConsumer(apiKey, apiSecret);
+            AccessToken accessToken = new AccessToken(token, tokenSecret);
+            twitter.setOAuthAccessToken(accessToken);
+
+        } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static List<Status> findTweetsByUser(String username) throws TwitterException{
+    	return twitter.getUserTimeline(username);
+    }
+	
+	public static Status getLatestStatusByUser(String username) throws TwitterException {
+		return findTweetsByUser(username).get(0);
+	}
+	
+}
