06c10ec579517ff4c39d3ee03c3a2a82bcf2e8c0
Nils Schwinning <nils.schwinning@gmx.de>
2018-10-15 22:52:43 +0200

    Adds twitter functionality

--- src/main/java/de/bich/alexa/handlers/RecentScherbenHandler.java
+++ src/main/java/de/bich/alexa/handlers/RecentScherbenHandler.java
@@ -4,13 +4,23 @@ import com.amazon.speech.slu.Intent;
 import com.amazon.speech.speechlet.IntentRequest;
 import com.amazon.speech.speechlet.Session;
 import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.StandardCard;
 
 public class RecentScherbenHandler implements IntentHandler{
 
 	@Override
 	public SpeechletResponse handleIntent(Intent intent, IntentRequest request, Session session) {
 		// TODO Auto-generated method stub
-		return null;
+		PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+		speech.setText("Bla");
+		
+		StandardCard card = new StandardCard();
+		card.setTitle("Scherben");
+		card.setText("Blubb");
+		
+		return SpeechletResponse.newTellResponse(speech, card);
+		
 	}
 
 }
